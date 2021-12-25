aa50cae20a5087f7afa1b5508cdf0a786fbb2728
Swapnil <81177774+Now-Tiger@users.noreply.github.com>
2021-12-26 03:30:47 +0530

    gradient-error-elimination-simple

--- Sratch/error-update.py
+++ Sratch/error-update.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env pypy3
+#!/usr/bin pypy3
 
-# ------------------------------------ Compare + Learn = Update ------------------------------------
+# ---------------------------------- Compare + Learn = Update ----------------------------------
 # Compare: Does your network make
 # good predictions?
 # Let’s measure the error and find out!
@@ -15,7 +15,7 @@ print(f"error : {round(error, 3)}")   # -> 0.303
 print('-'*15)
 
 
-# -------- Simplest form of neural network ---------
+# ------------------- Simplest form of neural network -------------------
 
 weight = .1 
 lr = .1     # learning rate
@@ -34,24 +34,27 @@ pred = neuralNet(input, weight)
 error = (pred - true) ** 2 
 print(f"error : {round(error, 3)}")     # 0.023   
 
-# We need to move the weight so that we can minimize the errors
 print('-'*15)
+
+
+# --------------- Making a prediction with a higher weight and evaluating error ---------------
 
 lr = .1 
 p_up = neuralNet(input, weight + lr)
 error_up = (p_up - true) ** 2
 print(f"updated error : {round(error_up, 3)}")
 
-# Make predictions on lower learning rate.
-
 print('-'*15)
+
+
+# -------------------------- Make predictions on lower learning rate --------------------------
 
 lr = .01
 p_dn = neuralNet(input, weight + lr)
 error_dn = (p_dn - true) ** 2
 print(f"updated error : {round(error_dn, 3)}")  # 0.004  <- best updated
 
-# $ pypy ch3-gradient-descent.py 
+# $ pypy3 ch3-gradient-descent.py 
 # error : 0.303
 # ---------------      
 # error : 0.023        
