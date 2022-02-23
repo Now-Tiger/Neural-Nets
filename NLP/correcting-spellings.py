b10440290a976081d11bd14a9c0db382409884ff
Swapnil <81177774+Now-Tiger@users.noreply.github.com>
2022-02-23 23:24:04 +0530

    textblob for correcting spells

---
+++ NLP/correcting-spellings.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env/ 'conda':base
+
+import pandas as pd
+import nltk
+from textblob import TextBlob
+
+# ------------- Create data -----------------
+text=['Introduction to NLP',
+        'It is likely to be useful, to people ',
+        'Machine learning is the new electrcity', 
+        'R is good langauage',
+        'I like this book',
+        'I want more books like this']
+
+data = pd.DataFrame({'tweet': text})
+#print(data)
+
+data['tweet'].apply(lambda x: str(TextBlob(x).correct()))
+print(data)
